2bb903cda3281db4e66ca13e7e78f50ea77c8bd8
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-03-29 19:30:16 +0530

    adding all the practice set of how to find out broken links & getting its responce code by HttpConnection

---
+++ src/test/java/Selenium_Each_Module_Code_Practice/Finding_all_bokenlinks_in_webpage.java
@@ -0,0 +1,63 @@
+package Selenium_Each_Module_Code_Practice;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.asserts.SoftAssert;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Finding_all_bokenlinks_in_webpage {
+
+	public static void main(String[] args) throws MalformedURLException, IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		int count=1;
+		int no_of_links = driver.findElements(By.tagName("a")).size();
+		System.out.println("Total Number of links present in webpage :- " + no_of_links);
+
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		SoftAssert softAssert = new SoftAssert();
+
+		for (WebElement link : links) {
+			String url = link.getAttribute("href");
+			HttpURLConnection connection = null;
+			try {
+				connection = (HttpURLConnection) new URL(url).openConnection();
+				connection.setRequestMethod("HEAD");
+				connection.connect();
+				int responseCode = connection.getResponseCode();
+				System.out.println("Response Code for " + url + " :- " + responseCode);
+				if(responseCode>400)
+				{
+					count++;
+				}
+				softAssert.assertTrue(responseCode > 400,
+						"The link with Text '" + link.getText() + "' is broken with code " + responseCode);
+			
+				
+			} catch (Exception e) {
+				softAssert.fail("Exception occurred while checking the link: " + e.getMessage());
+			} finally {
+				if (connection != null) {
+					connection.disconnect();
+				}
+			
+			}
+		}
+
+		//softAssert.assertAll();
+		driver.quit();
+		System.out.println("Total No. of Broken links:- "+count );
+		System.out.println("End of program execution");
+
+	}
+
+}
---
+++ src/test/java/Selenium_Each_Module_Code_Practice/Working_with_calender_controls.java
@@ -0,0 +1,79 @@
+package Selenium_Each_Module_Code_Practice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Working_with_calender_controls {
+
+	public static void main(String[] args) throws InterruptedException {
+        String expectedFromDateStr = "01/20/2020";
+        String expectedToDateStr = "02/26/2020";
+        String expectedFromDate = "20";
+        String expectedToDate = "26";
+        String testUrl = "https://jqueryui.com/datepicker/#date-range";
+
+        /*System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+*/
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get(testUrl);
+        Thread.sleep(5000);
+
+        WebElement frame = driver.findElement(By.xpath("//*[@id='content']/iframe"));
+        driver.switchTo().frame(frame);
+
+        // Steps for the From Date
+        WebElement fromDP = driver.findElement(By.xpath("//input[@id='from']"));
+        fromDP.click();
+        Thread.sleep(5000);
+
+        WebElement fromMonth = driver.findElement(By.xpath("//div/select[@class='ui-datepicker-month']"));
+        Select selectedFromMonth = new Select(fromMonth);
+        selectedFromMonth.selectByVisibleText("Jan");
+        Thread.sleep(5000);
+
+        WebElement fromDay = driver.findElement(By.xpath("//td[not(contains(@class,'ui-datepicker-month'))]/a[text()='" + expectedFromDate + "']"));
+        fromDay.click();
+        Thread.sleep(10000);
+
+        // Steps for the To Date
+        WebElement toDP = driver.findElement(By.xpath("//input[@id='to']"));
+        toDP.click();
+        Thread.sleep(5000);
+
+        WebElement toMonth = driver.findElement(By.xpath("//div/select[@class='ui-datepicker-month']"));
+        Select selectedToMonth = new Select(toMonth);
+        selectedToMonth.selectByVisibleText("Feb");
+        Thread.sleep(5000);
+
+        WebElement toDay = driver.findElement(By.xpath("//td[not(contains(@class,'ui-datepicker-month'))]/a[text()='" + expectedToDate + "']"));
+        toDay.click();
+        Thread.sleep(10000);
+
+        // Verify whether the values are as expected
+        String selectedFromDateStr = fromDP.getAttribute("value");
+        String selectedToDateStr = toDP.getAttribute("value");
+
+        if (selectedFromDateStr.equals(expectedFromDateStr) && selectedToDateStr.equals(expectedToDateStr)) {
+            System.out.println("Unit Test of jQuery Calendar passed");
+        } else {
+            System.out.println("Unit Test of jQuery Calendar failed");
+        }
+
+        driver.quit();
+        System.out.println("End of Program Execution");
+}
+}
