f3b611245ec5756038b73776d41ae09b7633f886
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-09 19:45:29 +0530

    Practice set of windows authentication pop-ups

---
+++ src/test/java/Handling_Windows_Based_Authentication_Popups/handing_window_based_authentications.java
@@ -0,0 +1,26 @@
+package Handling_Windows_Based_Authentication_Popups;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class handing_window_based_authentications {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		//Working with windows based authentication pop-ups
+		driver.findElement(By.xpath("//a[contains(text(),'Basic Auth')]")).click();
+		Thread.sleep(2000);
+		
+		driver.get("https://admin:admin@the-internet.herokuapp.com/");
+		
+		driver.close();
+		System.out.println("End of Program Execution");
+	}
+
+}
