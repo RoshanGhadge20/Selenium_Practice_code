3d0f1c9507a61b16b148d64dfcd8cb9cc5e80bb4
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-21 16:45:57 +0530

    Practice set for taking screenshot

---
+++ src/test/java/Selenium_Each_Module_Code_Practice/Take_Screenhshot.java
@@ -0,0 +1,30 @@
+package Selenium_Each_Module_Code_Practice;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Take_Screenhshot {
+	public static void main(String[] args) throws IOException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File dest = new File("E:\\Selenium_Practice_Code_learning\\Selenium_practice_code\\Screenshots\\demo1.png");
+		FileUtils.copyFile(source, dest);
+
+		driver.close();
+		System.out.println("End of program execution");
+	}
+}
