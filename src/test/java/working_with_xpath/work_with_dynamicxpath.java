5ac5af01b8863054584f8e341573e389863c031e
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-05-05 19:43:43 +0530

    practice set of xpaths & css selectors

---
+++ src/test/java/working_with_xpath/work_with_dynamicxpath.java
@@ -0,0 +1,38 @@
+package working_with_xpath;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class work_with_dynamicxpath {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Working with Dynamic xpaths
+		/*
+		 * 1. working with or operator driver.findElement(By.
+		 * xpath("//tagname[@attribute='value' or @attribute='value']"));
+		 * driver.findElement(By.
+		 * xpath("//tagname[@attribute='value' and @attribute='value']"));
+		 * 
+		 * 2. Contains method
+		 * driver.findElement(By.xpath("//tagname[contains(@id,'value')]"));
+		 * 
+		 * 3. Relative Xpath
+		 * driver.findElement(By.xpath("//tagname[@attribute='value']"));
+		 * 
+		 * 4. Text method driver.findElement(By.xpath("//tagname[text()='value']"));
+		 * 
+		 * 5. Start-with
+		 * driver.findElement(By.xpath("//tagname(start-with(@id,'value'))"));
+		 * 
+		 * 6. Chained xpath driver.findElement(By.
+		 * xpath("//tagname[@attribute='Value' //tagname[@attribute='value']"))
+		 * 
+		 */
+	}
+
+}
---
+++ src/test/java/working_with_xpath/work_with_xpath.java
@@ -0,0 +1,41 @@
+package working_with_xpath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class work_with_xpath {
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+
+		// Working with basic of xpaths
+		driver.findElement(By.id("name")).sendKeys("Roshan Ghadge");
+		// driver.findElement(By.className("")).sendKeys("");
+		// driver.findElement(By.name("")).sendKeys("");
+		driver.findElement(By.partialLinkText("New Browser")).click();
+		driver.findElement(By.linkText("New Browser Window")).click();
+		driver.findElement(By.tagName("input")).click();
+
+		// Working with types xpaths -> Absolute
+		/*
+		 * 1. Absolute xpath 2. Relative xpath
+		 * 
+		 * driver.findElement(By.xpath("//tagname[@attribute='value']));
+		 * driver.findElement(By.xpath("Abosulte xpath")
+		 * 
+		 * 
+		 * 
+		 */
+		driver.close();
+		System.out.println("End of program code");
+
+	}
+
+}
