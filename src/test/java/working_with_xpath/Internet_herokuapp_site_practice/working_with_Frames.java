2d48f9f30667d3f8ee6f2691e93f76c451c778de
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-05-12 12:12:54 +0530

    changes are made

--- src/test/java/working_with_xpath/Internet_herokuapp_site_practice/working_with_Frames.java
+++ src/test/java/working_with_xpath/Internet_herokuapp_site_practice/working_with_Frames.java
@@ -1,8 +1,11 @@
 package working_with_xpath.Internet_herokuapp_site_practice;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -27,17 +30,58 @@ public class working_with_Frames {
 		
 		
 		//Working with Geolocations
-		driver.findElement(By.linkText("Geolocation"));
-		driver.findElement(By.xpath("//button[text()='Where am I?']")).click();
-		String lattitude=driver.findElement(By.cssSelector("div[id='lat-value']")).getText();
-		System.out.println("Lattitude details "+lattitude);
-		String longitude=driver.findElement(By.cssSelector("div[id='long-value']")).getText();
-		System.out.println("Longitude details "+longitude);
+		/*
+		 * driver.findElement(By.linkText("Geolocation"));
+		 * driver.findElement(By.xpath("//button[text()='Where am I?']")).click();
+		 * String
+		 * lattitude=driver.findElement(By.cssSelector("div[id='lat-value']")).getText()
+		 * ; System.out.println("Lattitude details "+lattitude); String
+		 * longitude=driver.findElement(By.cssSelector("div[id='long-value']")).getText(
+		 * ); System.out.println("Longitude details "+longitude);
+		 */
+		
+		// Working with Horizontal Slider
+		System.out.println("Working with Horizontal Slider");
+		driver.findElement(By.linkText("Horizontal Slider")).click();		
+		WebElement sliderElement=driver.findElement(By.xpath("//input[@onchange='showValue(this.value)']"));
+		Actions action=new Actions(driver);
+		action.clickAndHold(sliderElement);
+		action.moveByOffset(40, 0).build().perform();
+		driver.navigate().back();
+		
+		
+		//Working with Hovers
+		driver.findElement(By.linkText("Hovers")).click();
+		WebElement imgElement= driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]"));
+		action.moveToElement(imgElement);
+		System.out.println("Working with mouse hover completed");
+		driver.navigate().back();
+		
+		//Working with Infinite Scroll
+		driver.findElement(By.linkText("Infinite Scroll")).click();
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(350,0)", "");
+		driver.navigate().back();
+		System.out.println("Page scrolled sucessfully");
+		
+		//working with Inputs
+		//we can perform this by passing values into the sendkeys method
+		
+		
+		//Working with Jquery Menu - Need to check it once 
+		
+		//Working with Javascript Alerts by driver.switchto().alert().accept();
+		
+		//Working with JavaScript onload event error
+		
+		//Working with Key Presses
+		driver.findElement(By.linkText("Key Presses")).click();
+		
+	
 		
 		
 		
-		
-		driver.close();
+		driver.quit();
 		System.out.println("End of program code execution");
 	}
 }
