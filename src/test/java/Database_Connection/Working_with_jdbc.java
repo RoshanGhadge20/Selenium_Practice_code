278b277700f4fc7d53d82422e05a22b2fe21c2eb
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-05-21 09:30:52 +0530

    practice set for webtables

--- src/test/java/Database_Connection/Working_with_jdbc.java
+++ src/test/java/Database_Connection/Working_with_jdbc.java
@@ -9,25 +9,24 @@ import java.sql.Statement;
 public class Working_with_jdbc {
 
 	public static void main(String[] args) throws SQLException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub	
 
 		Connection con = DriverManager.getConnection("jdbc:mysql://" + "localhost" + ":" + "3306" + "/qadbt", "root",
 				"root@123");
 		Statement s = con.createStatement();
 		ResultSet rs = s.executeQuery("select * from Employeeinfo");
-		  while (rs.next()) {
-              System.out.println("Name: " + rs.getString("name"));
-              System.out.println("ID: " + rs.getInt("id"));
-              System.out.println("Location: " + rs.getString("location"));
-              System.out.println("Age: " + rs.getInt("age")); 
-              System.out.println("****************************");
-          } 
-		  rs.close();
-          s.close();
-          con.close();
+		while (rs.next()) {
+			System.out.println("Name: " + rs.getString("name"));
+			System.out.println("ID: " + rs.getInt("id"));
+			System.out.println("Location: " + rs.getString("location"));
+			System.out.println("Age: " + rs.getInt("age"));
+			System.out.println("****************************");
+		}
+		rs.close();
+		s.close();
+		con.close();
+		System.out.println("End of program code execution");
+
+	}
 
 }
-
-}
-
-
---
+++ src/test/java/Selenium_Each_Module_Code_Practice/practice_of_webtables.java
@@ -0,0 +1,50 @@
+package Selenium_Each_Module_Code_Practice;
+
+import java.util.Iterator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class practice_of_webtables {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://testautomationpractice.blogspot.com/");
+	driver.manage().window().maximize();
+	
+	//Working with webtables 
+	int row_count=driver.findElements(By.xpath("//table[@name='BookTable']/tbody/tr")).size();
+	System.out.println("Total Number of rows into the table are :- "+ row_count);
+	
+	int col_count=driver.findElements(By.xpath("//table[@name='BookTable']/tbody/tr/th")).size();
+	System.out.println("Total Number of col into the table are :-"+col_count);
+	
+	/*
+	 * //Printing all the records form table for ( int i=1;i<=row_count;i++) {
+	 * for(int j=1;j<=col_count;j++) { System.out.print(driver.findElement(By.xpath(
+	 * "//table[@name='BookTable']/tbody/tr["+i+"]/td["+j+"]")).getText()); }
+	 * System.out.println(" "); }
+	 * 
+	 */
+	
+	 for (int j = 1; j <= col_count; j++) {
+         System.out.print(driver.findElement(By.xpath("//table[@name='BookTable']/tbody/tr[1]/th[" + j + "]")).getText() + "\t");
+     }
+     System.out.println();
+     
+     // Print the data rows
+     for (int i = 2; i <= row_count; i++) {
+         for (int j = 1; j <= col_count; j++) {
+             System.out.print(driver.findElement(By.xpath("//table[@name='BookTable']/tbody/tr[" + i + "]/td[" + j + "]")).getText() + "\t");
+         }
+         System.out.println();
+     }
+     
+	
+	driver.quit();
+	System.out.println("End of Program code execution");
+}
+}
