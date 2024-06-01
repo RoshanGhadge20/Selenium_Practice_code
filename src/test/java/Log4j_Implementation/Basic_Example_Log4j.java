82d88a732e18821b175c8a5d689316bac90b1f0e
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-01 20:43:54 +0530

    log4j implementation

---
+++ src/test/java/Log4j_Implementation/Basic_Example_Log4j.java
@@ -0,0 +1,22 @@
+package Log4j_Implementation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basic_Example_Log4j {
+    private static final Logger logg = LogManager.getLogger(Basic_Example_Log4j.class);
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        logg.info("Webdriver initiated");
+       
+
+        driver.quit();
+        logg.info("Driver object closes all the browser windows/tabs");
+        //System.out.println("End of program execution");
+    }
+}
