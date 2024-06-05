305a3cd9b3094ce12f0b37bc8da3f4130ad4eb32
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-05 21:05:44 +0530

    dynamic options are loading content

---
+++ src/test/java/Selenium_Each_Module_Code_Practice/Dynamic_Options.java
@@ -0,0 +1,42 @@
+package Selenium_Each_Module_Code_Practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Dynamic_Options {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		// Working with dynamic elements
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("q")).sendKeys("javatpoint");
+
+		// Wait for suggestions to load
+		Thread.sleep(2000);
+
+		List<WebElement> elements = driver.findElements(By.cssSelector("ul[role='listbox'] li"));
+
+		// Navigating through the all elements & clicking if & only if javatpoint option
+		// is available
+		for (int i = 0; i < elements.size(); i++) {
+			System.out.println("Options available: " + elements.get(i).getText());
+			if (elements.get(i).getText().equals("javatpoint")) {
+				elements.get(i).click();
+				break;
+			}
+		}
+
+		Thread.sleep(5000);
+		driver.quit();
+		System.out.println("End of program code execution");
+	}
+}
