7d5fa26056a5b2878cf3344e9fa536df554abc0b
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-17 20:43:51 +0530

    practice set of excel sheet

--- src/test/java/Selenium_Each_Module_Code_Practice/Working_wth_Excels.java
+++ src/test/java/Selenium_Each_Module_Code_Practice/Working_wth_Excels.java
@@ -2,60 +2,63 @@ package Selenium_Each_Module_Code_Practice;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.commons.collections4.functors.WhileClosure;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Working_wth_Excels {
 	public static void main(String[] args) throws IOException {
-		
 
-/**
- * The Working_wth_Excels class demonstrates how to work with Excel files
- * using the Apache POI library in a Selenium test automation context.
- * This class includes reading an Excel file and extracting data for data-driven testing.
- *
- * <p>Note: The Selenium WebDriver setup code is commented out and not in use in this example.
- *
- * <pre>
- * Usage:
- * 1. Update the file path to your local Excel file location.
- * 2. Uncomment the Selenium WebDriver setup and usage if needed.
- * </pre>
- *
- * Dependencies:
- * - Apache POI library for handling Excel files.
- * - Selenium WebDriver (if used for browser automation).
- *
- * Example of reading data from an Excel sheet:
- * <pre>
- * {@code
- * // Load the Excel file
- * FileInputStream fin = new FileInputStream("path_to_your_excel_file.xlsx");
- * XSSFWorkbook workbook = new XSSFWorkbook(fin);
- *
- * // Get the first sheet
- * XSSFSheet sheet = workbook.getSheetAt(0);
- *
- * // Read data from the sheet
- * DataFormatter formatter = new DataFormatter();
- * for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
- *     XSSFRow row = sheet.getRow(i);
- *     for (int j = 0; j < row.getLastCellNum(); j++) {
- *         XSSFCell cell = row.getCell(j);
- *         System.out.print(formatter.formatCellValue(cell) + " ");
- *     }
- *     System.out.println();
- * }
- * }
- * </pre>
- */
-		
-		
-		
+
+		/**
+		 * The Working_wth_Excels class demonstrates how to work with Excel files
+		 * using the Apache POI library in a Selenium test automation context.
+		 * This class includes reading an Excel file and extracting data for data-driven testing.
+		 *
+		 * <p>Note: The Selenium WebDriver setup code is commented out and not in use in this example.
+		 *
+		 * <pre>
+		 * Usage:
+		 * 1. Update the file path to your local Excel file location.
+		 * 2. Uncomment the Selenium WebDriver setup and usage if needed.
+		 * </pre>
+		 *
+		 * Dependencies:
+		 * - Apache POI library for handling Excel files.
+		 * - Selenium WebDriver (if used for browser automation).
+		 *
+		 * Example of reading data from an Excel sheet:
+		 * <pre>
+		 * {@code
+		 * // Load the Excel file
+		 * FileInputStream fin = new FileInputStream("path_to_your_excel_file.xlsx");
+		 * XSSFWorkbook workbook = new XSSFWorkbook(fin);
+		 *
+		 * // Get the first sheet
+		 * XSSFSheet sheet = workbook.getSheetAt(0);
+		 *
+		 * // Read data from the sheet
+		 * DataFormatter formatter = new DataFormatter();
+		 * for (int i = 0; i < sheet.getPhysicalNumberOfRows(); i++) {
+		 *     XSSFRow row = sheet.getRow(i);
+		 *     for (int j = 0; j < row.getLastCellNum(); j++) {
+		 *         XSSFCell cell = row.getCell(j);
+		 *         System.out.print(formatter.formatCellValue(cell) + " ");
+		 *     }
+		 *     System.out.println();
+		 * }
+		 * }
+		 * </pre>
+		 */
+
+
+
 		/*
 		 * WebDriverManager.chromedriver().setup(); WebDriver driver=new ChromeDriver();
 		 * driver.manage().window().maximize();
@@ -87,5 +90,40 @@ public class Working_wth_Excels {
 		 * System.out.println("End of Program Executions");
 		 */
 
+		// Working with Excel sheet for data driven testing FileInputStream fin = new
+		FileInputStream fin_input = new FileInputStream("E://Selenium_Practice_Code_learning//Selenium_practice_code//Excel files//Book1.xlsx");
+		XSSFWorkbook workbook=new XSSFWorkbook(fin_input);
+		int num_sheet =workbook.getNumberOfSheets();
+		System.out.println("Number of Sheets :- "+ num_sheet);
+
+		XSSFSheet sheet=workbook.getSheet("Test");
+		int num_row=sheet.getLastRowNum();
+		System.out.println("Number of Rows :- "+ num_row);
+
+		XSSFRow row=sheet.getRow(0);
+		int col_count=row.getLastCellNum();
+		System.out.println("Number of Cells :- "+ col_count);
+
+		/*
+		 * XSSFCell cell=row.getCell(0); System.out.println("First Cell Value:- "+
+		 * cell);
+		 */
+		DataFormatter formatter=new DataFormatter();
+		Iterator<Row> rowsIterator=sheet.iterator();
+		while (rowsIterator.hasNext())
+		{
+			Row row_value=rowsIterator.next();
+			Iterator<Cell> cellIterator=row_value.iterator();
+			while(cellIterator.hasNext())
+			{
+			
+				Cell cell_value=cellIterator.next();
+				System.out.println(formatter.formatCellValue(cell_value));
+			}
+			System.out.println("");
 		}
+
+
+
+	}
 }
