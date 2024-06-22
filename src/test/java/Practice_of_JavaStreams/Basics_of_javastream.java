7818b7786393122120252409fb38e8eef8c3b08d
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-22 13:02:13 +0530

    making few changes

--- src/test/java/Practice_of_JavaStreams/Basics_of_javastream.java
+++ src/test/java/Practice_of_JavaStreams/Basics_of_javastream.java
@@ -1,6 +1,8 @@
 package Practice_of_JavaStreams;
 import java.util.ArrayList;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
 public class Basics_of_javastream {
 
 	public static void main(String[] args) {
@@ -21,7 +23,21 @@ public class Basics_of_javastream {
 	     .filter(s -> s.startsWith(s)) // This filter condition doesn't make sense. It's checking if each string starts with itself. Assuming you want to filter strings that start with a specific prefix, you need to provide that prefix.
 	     .map(s -> s.toUpperCase()) // This line is fine, but you can simplify it using method reference.
 	     .forEach(System.out::println); // Here you need to use System.out::println to correctly reference the println method of the System.out object.
-
-	}
+		
+		
+		ArrayList<Integer> ageArrayList=new ArrayList<Integer>();
+		ageArrayList.add(20);
+		ageArrayList.add(10);
+		ageArrayList.add(250);
+		ageArrayList.add(30);
+		ageArrayList.add(50);
+		
+		//Practice set of javaStream Classes 
+		long a=ageArrayList.stream().count();
+		System.out.println("Total Number of count"+a);
+		System.out.println(ageArrayList.stream().distinct().sorted());
+		
+		}
+	
 
 }
