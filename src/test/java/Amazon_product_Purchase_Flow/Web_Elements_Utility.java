159697221ed54ff931297f11e3b073e36e06f438
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-25 19:06:00 +0530

    Converted test with the testng framework

--- src/test/java/Amazon_product_Purchase_Flow/Web_Elements_Utility.java
+++ src/test/java/Amazon_product_Purchase_Flow/Web_Elements_Utility.java
@@ -1,5 +1,6 @@
 package Amazon_product_Purchase_Flow;
 
+import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -7,55 +8,68 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Web_Elements_Utility 
-{
+public class Web_Elements_Utility {
 	WebDriver driver;
+
 	/**
 	 * Syntax to write down the webelement using findby
 	 * 
 	 * @FindBy(xpath="") WebElement element_name;
 	 * @FindBy(how=How.css, using="") WebElement element_name;
 	 */
-	
-	//Created a constructor for pagefactory all element initialization
-	public Web_Elements_Utility(WebDriver driver)
-	{
-		this.driver=driver;
-		PageFactory.initElements(driver,this);
+
+	// Created a constructor for pagefactory all element initialization
+	public Web_Elements_Utility(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 
-	//Keeping all the Web_Element store into this file 
-	
-	@FindBy(css = "input#ap_email") WebElement login_FieldElement;
-	@FindBy(css = "input.a-button-input") WebElement login_btnElement;
-	@FindBy(css = "input[name='password']") WebElement pass_FieldElement;
-	@FindBy(css = "input[type='submit']") WebElement sign_btnElement;
-	@FindBy(css ="input#twotabsearchtextbox") WebElement search_fieldElement;
-	@FindBy(css ="input#nav-search-submit-button") WebElement search_iconElement;
-	@FindBy (css ="div[data-cy='title-recipe']") WebElement list_ofproductsElement;
-	@FindBy(css = "div.s-suggestion-container") WebElement suggestion_Element;
-	
-	public void login_with_mobile() 
-	{
+	// Keeping all the Web_Element store into this file
+
+	@FindBy(css = "input#ap_email")
+	WebElement login_FieldElement;
+	@FindBy(css = "input.a-button-input")
+	WebElement login_btnElement;
+	@FindBy(css = "input[name='password']")
+	WebElement pass_FieldElement;
+	@FindBy(css = "input[type='submit']")
+	WebElement sign_btnElement;
+	@FindBy(css = "input#twotabsearchtextbox")
+	WebElement search_fieldElement;
+	@FindBy(css = "input#nav-search-submit-button")
+	WebElement search_iconElement;
+	@FindBy(css = "div[data-cy='title-recipe']")
+	WebElement list_ofproductsElement;
+	@FindBy(css = "div.s-suggestion-container")
+	WebElement suggestion_Element;
+
+	public void login_with_mobile() {
 		login_FieldElement.sendKeys("8767003565");
 		login_btnElement.click();
 		pass_FieldElement.sendKeys("roshan2000");
 		sign_btnElement.click();
 	}
-	
-	public void Search_Product() throws InterruptedException 
-	{
-	search_fieldElement.sendKeys("Realme 10 Pro");
-	//Thread.sleep(5000);
-	
-	List<WebElement> sugg_prod=driver.findElements(By.cssSelector("div.s-suggestion-container"));
-	for(WebElement element: sugg_prod)
-	{
-		System.out.println("List of suggested products are:- "+ element.getText() );
+
+	public void Search_Product() throws InterruptedException {
+		search_fieldElement.sendKeys("Realme 10 Pro");
+		// Thread.sleep(5000);
+
+		/*
+		 * getting an StaleElementReferenceException over here while executing the code
+		 * Sol:- 1 ( Thread.sleep(3000)) Sol:- 2 ( implement webdriver wait over here (
+		 * explicit)
+		 */
+		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement ele = w
+				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.s-suggestion-container")));
+		List<WebElement> sugg_prod = driver.findElements(By.cssSelector("div.s-suggestion-container"));
+		for (WebElement element : sugg_prod) {
+			System.out.println("List of suggested products are:- " + element.getText());
+		}
+		search_iconElement.click();
 	}
-	search_iconElement.click();
-	}
-	
-	
+
 }
--- src/test/java/Amazon_product_Purchase_Flow/amazon_product_buy.java
+++ src/test/java/Amazon_product_Purchase_Flow/amazon_product_buy.java
@@ -7,72 +7,77 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-public class amazon_product_buy 
-{
-	public static void main(String[] args) throws InterruptedException
-	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
 
-		//Implemenataion of Implicit wait 
-		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
-		
-		//creating obj for base utility class
-		Web_Elements_Utility objElements_Utility=new Web_Elements_Utility(driver);
+public class amazon_product_buy {
 
-		
-		driver.manage().window().maximize();
-		driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26useRedirectOnSuccess%3D1%26signIn%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
-		
-		
-		//Clearing cookies & other data before execution of test 
-		/*
-		 * driver.manage().deleteAllCookies();
-		 * System.out.println("All the cookies are cleared");
-		 */
-		
-		//login_with_mobile_number& password	
-		objElements_Utility.login_with_mobile();
-		System.out.println("******* Test 1 Passed ******");
-		
-		//Gettting page title & verify it 
-		String exp_title="Your Amazon.in";
-		String act_title=driver.getTitle();
-		System.out.println(act_title);
-		if (exp_title.equals(act_title)) 
-		{
-			System.out.println("Title of page :- "+ act_title);
-		}
-		else 
-		{
-			System.out.println("Fail, Page title does not matches");
-		}
-		System.out.println("******* Test 2 Passed ******");
-		
-		
-		//Searching Product
-		objElements_Utility.Search_Product();
-		System.out.println("******* Test 3 *********");
-		
-		//Finding the list of product
-		List<WebElement> pro_element=  driver.findElements(By.cssSelector("div[data-cy='title-recipe']"));
-		for(WebElement product : pro_element)
-		{
-			System.out.println(product.getText());	
-		}
-		System.out.println("****** Test 4 *******");
-		
-		
-		
-		
-		
-		
-		
-		driver.quit();
-		System.out.println("End Of program execution");
+    static WebDriver driver;
+    static Web_Elements_Utility objElements_Utility;
 
-	}
+    @BeforeClass
+    public static void tear_up() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26useRedirectOnSuccess%3D1%26signIn%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+    }
 
+    @Test(priority = 1)
+    public static void implement_wait() {
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test(priority = 2)
+    public static void Making_obj_of_utility() {
+        objElements_Utility = new Web_Elements_Utility(driver);
+    }
+
+    @Test(priority = 3)
+    public static void login_with_mobile_number() {
+        // login_with_mobile_number& password
+        objElements_Utility.login_with_mobile();
+        System.out.println("******* Test 1 Passed ******");
+    }
+
+    @Test(priority = 4)
+    public static void Verify_page_title() {
+        // Getting page title & verify it
+        String exp_title = "Your Amazon.in";
+        String act_title = driver.getTitle();
+        System.out.println(act_title);
+        if (exp_title.equals(act_title)) {
+            System.out.println("Title of page :- " + act_title);
+        } else {
+            System.out.println("Fail, Page title does not match");
+        }
+       // System.out.println("******* Test 2 Passed ******");
+    }
+
+    @Test(priority = 5)
+    public static void Searching_product() throws InterruptedException {
+        objElements_Utility.Search_Product();
+       // System.out.println("******* Test 3 *********");
+    }
+
+    @Test(priority = 6)
+    public static void Findind_list_of_prod() {
+        List<WebElement> pro_element = driver.findElements(By.cssSelector("div[data-cy='title-recipe']"));
+        // Finding the list of products
+        for (WebElement product : pro_element) {
+           System.out.println(product.getText());
+        }
+        //System.out.println("****** Test 4 *******");
+    }
+
+    @AfterClass
+    public static void Tear_down() {
+        if (driver != null) {
+            driver.quit();
+        }
+        //System.out.println("End Of program execution");
+    }
 }
