8dc812c0596083a828a9761ea68bbc3a4371a146
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-28 15:53:46 +0530

    implemented log4j

--- src/test/java/Amazon_product_Purchase_Flow/amazon_product_buy.java
+++ src/test/java/Amazon_product_Purchase_Flow/amazon_product_buy.java
@@ -2,13 +2,17 @@ package Amazon_product_Purchase_Flow;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class amazon_product_buy {
@@ -16,13 +20,16 @@ public class amazon_product_buy {
 	static WebDriver driver;
 	static Web_Elements_Utility objElements_Utility;
 
+	private static final Logger logg = LogManager.getLogger(amazon_product_buy.class);
+
 	@BeforeClass
 	public static void tear_up() {
 		WebDriverManager.chromedriver().setup();
-		driver = new EdgeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(
 				"https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26useRedirectOnSuccess%3D1%26signIn%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0");
+		logg.info("Webdriver initiated");
 	}
 
 	@Test(priority = 1)
@@ -33,6 +40,7 @@ public class amazon_product_buy {
 	@Test(priority = 2)
 	public static void Making_obj_of_utility() {
 		objElements_Utility = new Web_Elements_Utility(driver);
+		logg.info("object of utility class is created");
 	}
 
 	@Test(priority = 3)
