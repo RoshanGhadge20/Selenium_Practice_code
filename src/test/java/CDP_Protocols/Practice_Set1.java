57c0be4b350b381fe138e8b36690d89648b70fea
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-07-26 18:56:16 +0530

    practice set with CDP

---
+++ src/test/java/CDP_Protocols/Practice_Set1.java
@@ -0,0 +1,32 @@
+package CDP_Protocols;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v120.emulation.Emulation;  // Ensure this matches the CDP version
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Practice_Set1 {
+    public static void main(String[] args)
+    {
+        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+
+        // Initialization of Chrome DevTools
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+        devTools.createSession();
+        
+        int width = 375;  // Device width
+        int height = 812; // Device height
+        int deviceScaleFactor = 3; // Device scale factor (high DPI)
+        boolean isMobile = true; // Emulate mobile device
+
+        devTools.send(Emulation.setDeviceMetricsOverride(
+                width, height, deviceScaleFactor, isMobile, null, null, null, null, null, null, null, null, null
+        ));
+        
+        driver.get("https://stackoverflow.com/questions/72423913/java-selenium-driver-getdevtools-is-undefined-for-the-type-chromedriver");
+
+        driver.quit();
+    }
+}
