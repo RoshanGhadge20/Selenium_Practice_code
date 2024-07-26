057217c0ed3b55173e7096c01b7ebb485235a748
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-07-26 19:00:43 +0530

    resolved issues into cdp program code for practice

--- src/test/java/CDP_Protocols/Practice_Set1.java
+++ src/test/java/CDP_Protocols/Practice_Set1.java
@@ -1,14 +1,18 @@
 package CDP_Protocols;
 
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.v120.emulation.Emulation;  // Ensure this matches the CDP version
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v122.emulation.Emulation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Practice_Set1 {
-    public static void main(String[] args)
-    {
-        ChromeOptions options = new ChromeOptions();
+	public static void main(String[] args) 
+	{
+		ChromeOptions options = new ChromeOptions();
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(options);
 
@@ -22,11 +26,11 @@ public class Practice_Set1 {
         boolean isMobile = true; // Emulate mobile device
 
         devTools.send(Emulation.setDeviceMetricsOverride(
-                width, height, deviceScaleFactor, isMobile, null, null, null, null, null, null, null, null, null
+                width, height, deviceScaleFactor, isMobile, null, null, null, null, null, null, null, null, null, java.util.Optional.empty()
         ));
         
         driver.get("https://stackoverflow.com/questions/72423913/java-selenium-driver-getdevtools-is-undefined-for-the-type-chromedriver");
 
         driver.quit();
-    }
+	}
 }
