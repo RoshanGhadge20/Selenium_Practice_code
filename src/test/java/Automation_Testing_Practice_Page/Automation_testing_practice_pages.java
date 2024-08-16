52f92f6403f7650b956665bbf7878a54052c6fee
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-08-16 19:18:39 +0530

    practice set with webtables

--- src/test/java/Automation_Testing_Practice_Page/Automation_testing_practice_pages.java
+++ src/test/java/Automation_Testing_Practice_Page/Automation_testing_practice_pages.java
@@ -1,9 +1,14 @@
 package Automation_Testing_Practice_Page;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class Automation_testing_practice_pages 
 {
@@ -29,6 +34,25 @@ public class Automation_testing_practice_pages
 	@FindBy(css = "input[id=sunday]")
 	WebElement dayfield;
 	
+	@FindBy(css="select[id='country']")
+	WebElement drpdwnfield;
+	
+	@FindBy(css = "select.form-control[id='colors'] option")
+	List<WebElement> colors;
+	
+	//List<WebElement> colors=driver.findElements(By.cssSelector("select.form-control[id='colors'] option"));
+	
+	@FindBy(css = "input#datepicker")
+	WebElement datepicker;
+	
+	@FindBy(xpath = "//a[normalize-space()='16'][1]")
+	WebElement today_date;
+	
+	@FindBy(css = "table[name='BookTable'] tbody tr th")
+	List<WebElement> table_heding;
+	
+	@FindBy(css = "table[name='BookTable'] tbody tr td")
+	List<WebElement> table_data;
 	
 	
 	
@@ -52,6 +76,61 @@ Automation_testing_practice_pages(WebDriver driver)
 		genderfield.click();
 		dayfield.click();
 	}
+	
+	public void select_country() 
+	{
+		Select sc=new Select(drpdwnfield);
+		List<WebElement> options=sc.getOptions();
+		for(WebElement option:options)
+		{
+			if(option.getText().contains("India"))
+			{
+				option.click();
+			}
+		}
+		
+	}
+	
+	public void fetch_colors()
+	{
+		  Iterator<WebElement> it=colors.iterator();
+		  while(it.hasNext())
+		  {
+			  System.out.println((it.next()).getText());
+		  }
+	}
+	
+	public void today_date_pick() throws InterruptedException
+	{
+		datepicker.click();
+		Thread.sleep(2000);
+		today_date.click();
+	}
+	
+	public void table_heading()
+	{
+		Iterator<WebElement> th=table_heding.iterator();
+		  while(th.hasNext())
+		  {
+			  System.out.print(((th.next()).getText())+" ");
+		  }
+		  System.out.println();
+	}
+	public void table_data()
+	{
+		Iterator<WebElement> td=table_data.iterator();
+		while(td.hasNext())
+		{
+			System.out.print(((td.next()).getText())+" ");
+			System.out.println("");
+			
+		}
+		//System.out.println(" ");
+	}
+
+
+	
+	
 	
 	
 }
--- src/test/java/Automation_Testing_Practice_Page/Practice_of_each_section_of_page.java
+++ src/test/java/Automation_Testing_Practice_Page/Practice_of_each_section_of_page.java
@@ -1,13 +1,15 @@
 package Automation_Testing_Practice_Page;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Practice_of_each_section_of_page 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
 		// Practice set to working with each section - https://www.browserstack.com/guide/page-object-model-in-selenium
 
@@ -17,12 +19,36 @@ public class Practice_of_each_section_of_page
 		driver.manage().window().maximize();
 
 		Automation_testing_practice_pages obj=new Automation_testing_practice_pages(driver);
-		
+
 		System.out.println("Title of the page :- "+driver.getTitle());
-		
+
+		//Working with form details with sendkeys method
 		obj.fill_details();
 
+		// Working with select dropdown
+		obj.select_country();
+
+		//working with colors
+		obj.fetch_colors();
+
+		//Working with calender 
+		obj.today_date_pick();
+
+		//Working with navigate forward/backword
+		WebElement orangeHRM=driver.findElement(By.xpath("//a[text()='orange HRM']"));
+		orangeHRM.click();
+		Thread.sleep(3000);
+		driver.navigate().back();
 		
+		//Working with webtables 
+		System.out.println("-------Printing WebTableTable Data-------");
+		obj.table_heading();
+		obj.table_data();
+		
+		
+
+
+
 
 
 		driver.quit();
