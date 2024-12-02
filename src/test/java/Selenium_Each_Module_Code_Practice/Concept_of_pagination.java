60ca4c40b8ce840d135f8305c164d4c1c2d33860
Roshan_Ghadge <roshanghadge568@gmail.com>
2024-12-02 22:51:45 +0530

    REformatted Code

--- src/test/java/Selenium_Each_Module_Code_Practice/Concept_of_pagination.java
+++ src/test/java/Selenium_Each_Module_Code_Practice/Concept_of_pagination.java
@@ -9,35 +9,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Concept_of_pagination 
-{
-	public static void main(String[] args) 
-	{
+public class Concept_of_pagination {
+	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver= new ChromeDriver();
-		
+		WebDriver driver = new ChromeDriver();
+
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().maximize();
-		
-		// Working with concepts of paginations 
-		
+
+		// Working with concepts of paginations
+
 		System.out.println("Paginations Concepts");
-		
-		List<WebElement> pages=driver.findElements(By.xpath("//ul[@id='pagination']/li"));
-		for(WebElement page: pages)
-		{
-			List<WebElement> tablecontent=driver.findElements(By.cssSelector("div[class='table-container'] tbody tr"));
-			for(WebElement tablerow: tablecontent)
-			{
+
+		List<WebElement> pages = driver.findElements(By.xpath("//ul[@id='pagination']/li"));
+		for (WebElement page : pages) {
+			List<WebElement> tablecontent = driver
+					.findElements(By.cssSelector("div[class='table-container'] tbody tr"));
+			for (WebElement tablerow : tablecontent) {
 				System.out.println(tablerow.getText());
 			}
 			System.out.println("----------");
 		}
-		
-		
-		
-		
-		
+
 		driver.quit();
 		System.out.println("End of program code");
 	}
--- src/test/java/Selenium_Each_Module_Code_Practice/Drag_and_Drop.java
+++ src/test/java/Selenium_Each_Module_Code_Practice/Drag_and_Drop.java
@@ -9,27 +9,24 @@ import org.openqa.selenium.interactions.Actions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Drag_and_Drop 
-{
-	public static void main(String[] args) 
-	{
+public class Drag_and_Drop {
+	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
-		ChromeOptions options=new ChromeOptions();
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--start-maximized");
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://testautomationpractice.blogspot.com/");
-		Actions act=new Actions(driver);
+		Actions act = new Actions(driver);
 		drap_and_dropped(driver, act);
 		driver.quit();
 		System.out.println("End of program Execution");
 	}
 
-	public static void drap_and_dropped(WebDriver driver, Actions act)
-	{
-		WebElement drag=driver.findElement(By.cssSelector("div#draggable"));
-		WebElement drop=driver.findElement(By.cssSelector("div#droppable"));
+	public static void drap_and_dropped(WebDriver driver, Actions act) {
+		WebElement drag = driver.findElement(By.cssSelector("div#draggable"));
+		WebElement drop = driver.findElement(By.cssSelector("div#droppable"));
 		act.dragAndDrop(drag, drop).build().perform();
-		String state=driver.findElement(By.cssSelector("div.ui-state-highlight")).getText();
+		String state = driver.findElement(By.cssSelector("div.ui-state-highlight")).getText();
 		System.out.println(state);
 	}
 
